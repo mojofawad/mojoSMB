ee949b29f5c0532be8c3f30eec1390d926fd9b80
joey fawad <mojofawad@gmail.com>
2024-07-02 22:19:46 -0400

    add postgresdb to AppHost

--- src/mojoSMB.Aspire/mojoSMB.AppHost/Program.cs
+++ src/mojoSMB.Aspire/mojoSMB.AppHost/Program.cs
@@ -4,9 +4,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.mojoSMB_Api>("api");
 
+// TODO: Add volume to the database container
+var db = builder.AddPostgres("postgres").AddDatabase("db");
+
 builder.AddProject<Projects.mojoSMB_Web>("web")
     .WithReference(api);
-
-// TODO: Figure out "failed to start executable" issue when launching from Rider in Fedora 40
 
 builder.Build().Run();
